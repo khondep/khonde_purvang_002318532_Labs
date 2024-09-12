45991ee2d29ea1776367097a20d123b70b0ac916
purvang khonde <khonde.p@northeastern.edu>
2024-09-12 16:03:08 -0400

    Completed lab0

---
+++ Lab0/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
